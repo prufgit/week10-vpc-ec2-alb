a40aa2c2acd85d00d7bc28d6bc1068da92a1bcea
Pruthvi <pruthvi.pediredla06@gmail.com>
2024-04-10 22:45:04 -0400

    new code

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "week10-p30-terraform"
+    key            = "week10/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "state-log"
+  }
+}
+
